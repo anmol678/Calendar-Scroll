2d0970f40d37c777e8c9e0e4fcb303094e6f6c96
Anmol Jain <anmolj678@gmail.com>
2024-02-13 14:55:48 -0800

    cleanup

--- CustomScrollAnimation/Helpers/Date+Extensions.swift
+++ CustomScrollAnimation/Helpers/Date+Extensions.swift
@@ -9,23 +9,6 @@ import SwiftUI
 
 /// Date Extensions
 extension Date {
-    static var currentMonth: Date {
-        let calendar = Calendar.current
-        guard let currentMonth = calendar.date(from: Calendar.current.dateComponents([.month, .year], from: .now)) else {
-            return .now
-        }
-        
-        return currentMonth
-    }
-    
-    func getDateFor(_ components: Calendar.Component...) -> Date {
-        let calendar = Calendar.current
-        guard let date = calendar.date(from: calendar.dateComponents(Set(components), from: self)) else {
-            return self
-        }
-        
-        return date
-    }
     
     func startOfWeek() -> Date {
         let calendar = Calendar.current
